68f423974bddb71bde7f73a27184cc7cc96becb0
amedakovic <medakovic.aleks1@gmail.com>
2022-10-03 19:09:48 +0200

    1

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Services.CharacterService;
