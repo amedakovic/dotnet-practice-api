e216422b40153567c4974b3586abfad89c5eacee
amedakovic <medakovic.aleks1@gmail.com>
2022-10-05 16:50:40 +0200

    Added hitting character with weapon

---
+++ Services/FightService/FightsService.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.FIght;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Services.FightService
+{
+    public class FightService : IFightService
+    {
+        private readonly DataContext _context;
+
+        public FightService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto weaponAttack)
+        {
+            var response = new ServiceResponse<AttackResultDto>();
+            try
+            {
+                var attacker = await _context.Characters
+                    .Include(c => c.Weapon)
+                    .FirstOrDefaultAsync(c=> c.Id == weaponAttack.AttackerId);
+                var opponent = await _context.Characters
+                    .Include(c => c.Weapon)
+                    .FirstOrDefaultAsync(c=> c.Id == weaponAttack.OpponentId);
+                
+                int damage = attacker.Weapon.Damage + (new Random().Next(attacker.Strength));
+                damage -= new Random().Next(opponent.Defeats);
+
+                if(damage > 0 ) opponent.HitPoints -= damage;
+
+                if(opponent.HitPoints <= 0) response.Message = $"{opponent.Name} has been defeated!";
+
+                await _context.SaveChangesAsync();
+
+                response.Data = new AttackResultDto{
+                    Attacker = attacker.Name,
+                    Opponent = opponent.Name,
+                    AttackerHP = attacker.HitPoints,
+                    OpponentHP = opponent.HitPoints,
+                    Damage = damage
+                };
+            }
+            catch(Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+    }
+}
